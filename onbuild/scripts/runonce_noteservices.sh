dad783bdd571d6553121c9313501c45e3b1930dc
陈金沛 <jpchen@lisystec.com>
2015-11-13 15:50:53 +0800

    test

--- onbuild/scripts/runonce_noteservices.sh
+++ onbuild/scripts/runonce_noteservices.sh
@@ -5,14 +5,14 @@ source /scripts/runonce_nodejs.sh
 runonce_nodejs
 
 NOTES_DBNAME=${NOTES_DBNAME:-notesmicroservices}
-NOTES_SESSION_SECRET=${STACK_SESSION_SECRET:-$NOTES_DBNAME}
+NOTES_SESSION_SECRET=${NOTES_SESSION_SECRET:-$NOTES_DBNAME}
 NODE_ENV=${NODE_ENV:-development}
 # get db user info from linked container
 NOTES_DBUSER=${DB_ENV_DBUSER:-}
 NOTES_DBPASS=${DB_ENV_DBPASS:-}
 
 #
-# Stack setup
+# Services setup
 #
 runonce_noteservices() {
     # Services main configs
@@ -20,9 +20,9 @@ runonce_noteservices() {
     sudo -u node -H sed -i "s/{{NOTES_SESSION_SECRET}}/${NOTES_SESSION_SECRET}/" ${APP_DIR}/config/env/default.js
     # db configs
     if [[ -n "DB_PORT_27017_TCP_ADDR}" ]]; then
-        sudo -u node -H sed -i "s/{{STACK_DBHOST}}/${DB_PORT_27017_TCP_ADDR}/" ${APP_DIR}/config/env/${NODE_ENV}.js
+        sudo -u node -H sed -i "s/{{NOTES_DBHOST}}/${DB_PORT_27017_TCP_ADDR}/" ${APP_DIR}/config/env/${NODE_ENV}.js
         # local.example.js
-        sudo -u node -H sed -i "s/{{STACK_DBHOST}}/${DB_PORT_27017_TCP_ADDR}/" ${APP_DIR}/config/env/local.example.js
+        sudo -u node -H sed -i "s/{{NOTES_DBHOST}}/${DB_PORT_27017_TCP_ADDR}/" ${APP_DIR}/config/env/local.example.js
     fi
     sudo -u node -H sed -i "s/{{NOTES_DBNAME}}/${NOTES_DBNAME}/" ${APP_DIR}/config/env/${NODE_ENV}.js
     sudo -u node -H sed -i "s/{{NOTES_DBUSER}}/${NOTES_DBUSER}/" ${APP_DIR}/config/env/${NODE_ENV}.js
