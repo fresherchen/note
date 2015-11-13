806ee717eaefd79c2e7b35c74e2518959d00e724
陈金沛 <jpchen@lisystec.com>
2015-11-13 14:55:15 +0800

    test

--- onbuild/scripts/custom.sh
+++ onbuild/scripts/custom.sh
@@ -10,6 +10,3 @@ if [[ -n "DB_PORT_27017_TCP_ADDR}" ]]; then
         sudo -u node -H sed -ri "s|mongodb://.*|mongodb://${DB_PORT_27017_TCP_ADDR}/${NOTES_DBNAME}',|g" ${APP_DIR}/config/env/local.js
     fi
 fi
-
-cd /${APP_DIR}/src
-node server.js
